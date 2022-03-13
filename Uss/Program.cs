38a5c43e7c88ab39dae8e013e590170bb2e64d0e
Admin <Admin@DESKTOP-C0SFP39>
2022-03-13 22:23:33 +0200

    Добавьте файлы проекта.

---
+++ Uss/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetBufferSize(80, 25);
+
+            // Отрисовка рамки
+            HorizonatallLine upline = new HorizonatallLine(0, 78, 0, '+');
+            HorizonatallLine downline = new HorizonatallLine(0, 78, 24, '+');
+            Verticalline leftLine = new Verticalline(0, 24, 0, '+');
+            Verticalline rightline = new Verticalline(0, 24, 78, '+');
+            upline.Drow();
+            downline.Drow();
+            leftLine.Drow();
+            rightline.Drow();
+
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*') ;
+            p.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Uss/Verticalline.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uss
+{
+    class Verticalline : Figure
+    {
+        public Verticalline(int yUp, int yDown, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+    }
+}
