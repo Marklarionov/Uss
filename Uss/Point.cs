012370a450bf3815359ee6fd638f9bc8074f22c9
Marklarionov <93324243+Marklarionov@users.noreply.github.com>
2022-03-21 00:24:08 +0200

    Update Point.cs

--- Uss/Point.cs
+++ Uss/Point.cs
@@ -8,25 +8,30 @@ namespace Uss
 {
     internal class Point
     {
+        //Класс для создания "точки"
         public int x;
         public int y;
         public char sym;
+
         public Point()
         {
 
         }
+
         public Point(int _x, int _y, char _sym)
         {
             x = _x;
             y = _y;
             sym = _sym;
         }
+
         public Point(Point p)
         {
-            x = p.x;
-            y = p.y;
+            x=p.x;
+            y=p.y; 
             sym = p.sym;
         }
+
         public void Move(int offset, Direction direction)
         {
             if (direction == Direction.RIGHT)
@@ -39,30 +44,27 @@ namespace Uss
             }
             else if (direction == Direction.UP)
             {
-                y = y - offset;
+                y = y + offset;
             }
             else if (direction == Direction.DOWN)
             {
-                y = y + offset;
+                y = y - offset;
             }
         }
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
-            Console.WriteLine(sym);
-        }
-        public override string ToString()
-        {
-            return x + "," + y + "," + sym;
+            Console.Write(sym);
         }
         public void Clear()
         {
             sym = ' ';
             Draw();
-        }
-        public bool IsHit(Point p)
-        {
-            return p.x == this.x && p.y == this.y;
         }
     }
 }
