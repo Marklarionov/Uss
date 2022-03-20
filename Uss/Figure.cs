c4eb83ac1ae8db1a2469fb0b4547c7d934c1b0de
Marklarionov <93324243+Marklarionov@users.noreply.github.com>
2022-03-21 00:22:51 +0200

    Update Figure.cs

--- Uss/Figure.cs
+++ Uss/Figure.cs
@@ -10,30 +10,33 @@ namespace Uss
     {
         protected List<Point> pList;
 
-        public void Drow()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
-            }
-        }
-		internal bool IsHit(Figure figure)
+	public void Draw()
+	{
+		foreach (Point p in pList)
 		{
-			foreach (var p in pList)
-			{
-				if (figure.IsHit(p))
-					return true;
-			}
-			return false;
+
+			p.Draw();
 		}
-		private bool IsHit(Point point)
+	}
+
+	internal bool IsHit(Figure figure)
+	{
+		foreach (var p in pList)
 		{
-			foreach (var p in pList)
-			{
-				if (p.IsHit(point))
-					return true;
-			}
-			return false;
+			if (figure.IsHit(p))
+
+				return true;
 		}
+		return false;
+	}
+
+	private bool IsHit(Point point)
+	{
+		foreach (var p in pList)
+		{
+			if (p.IsHit(point))
+				return true;
+		}
+		return false;
 	}
 }
